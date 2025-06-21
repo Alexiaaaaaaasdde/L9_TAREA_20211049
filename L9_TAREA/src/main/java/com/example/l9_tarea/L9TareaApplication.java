37edb9c5d6fcd6efc0947f31f74709a2f7472c83
AlexiaPT <apalaciost@pucp.edu.pe>
2025-06-21 16:59:56 -0500

    v1

---
+++ L9_TAREA/src/main/java/com/example/l9_tarea/L9TareaApplication.java
@@ -0,0 +1,13 @@
+package com.example.l9_tarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L9TareaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L9TareaApplication.class, args);
+    }
+
+}
